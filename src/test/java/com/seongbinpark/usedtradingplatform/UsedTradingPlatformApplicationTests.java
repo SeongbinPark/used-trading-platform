4c4ee3c28cded3b7928b310b24f41b9264f04aa4
SeongbinPark <binni0515@gmail.com>
2022-10-04 14:39:44 +0900

    [#1]프로젝트 생성

---
+++ src/test/java/com/seongbinpark/usedtradingplatform/UsedTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seongbinpark.usedtradingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedTradingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
