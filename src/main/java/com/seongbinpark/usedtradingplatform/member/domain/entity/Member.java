53907f878d173f3784feeecf2cb29348e0bde1f2
SeongbinPark <binni0515@gmail.com>
2022-10-04 15:34:32 +0900

    [#2]회원 엔티티 생성

---
+++ src/main/java/com/seongbinpark/usedtradingplatform/member/domain/entity/Member.java
@@ -0,0 +1,33 @@
+package com.seongbinpark.usedtradingplatform.member.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "MEMBER")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
