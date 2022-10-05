a2fa9fb12edf9d36e4b9ce99d057b6faf580c431
SeongbinPark <binni0515@gmail.com>
2022-10-05 14:43:18 +0900

    [#3] LoginService, SessionLoginService 생성 후 로그인 기능 추가(추후 JWT도 추가예정)

---
+++ src/main/java/com/seongbinpark/usedtradingplatform/member/service/SessionLoginService.java
@@ -0,0 +1,21 @@
+package com.seongbinpark.usedtradingplatform.member.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Service
+@RequiredArgsConstructor
+public class SessionLoginService implements LoginService {
+
+    private final HttpSession httpSession;
+    public static final String MEMBER_ID = "MEMBER_ID";
+
+
+    @Override
+    public void login(long id) {
+        httpSession.setAttribute(MEMBER_ID, id);
+    }
+}
