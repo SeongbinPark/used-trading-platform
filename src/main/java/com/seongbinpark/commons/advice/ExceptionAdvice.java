86386b32199d239e84e894d50981a658b76e7a80
SeongbinPark <binni0515@gmail.com>
2022-10-05 16:03:02 +0900

    [#4] UNAUTHORIZED 예외 ExceptionAdvice 에 등록,처리

--- src/main/java/com/seongbinpark/commons/advice/ExceptionAdvice.java
+++ src/main/java/com/seongbinpark/commons/advice/ExceptionAdvice.java
@@ -1,6 +1,8 @@
 package com.seongbinpark.commons.advice;
 
+import com.seongbinpark.commons.HttpStatusResponseEntity;
 import com.seongbinpark.usedtradingplatform.member.exception.MemberNotFoundException;
+import com.seongbinpark.usedtradingplatform.member.exception.UnauthenticatedAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +16,10 @@ public class ExceptionAdvice {
     @ExceptionHandler(MemberNotFoundException.class)
     public ResponseEntity<HttpStatus> memberNotFoundException() {
         return RESPONSE_NOT_FOUND;
+    }
+
+    @ExceptionHandler(UnauthenticatedAccessException.class)
+    public ResponseEntity<HttpStatus> unauthenticatedAccessException() {
+        return RESPONSE_UNAUTHORIZED;
     }
 }
