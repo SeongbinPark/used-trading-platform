85f228f8924dd88d8efcf1bd2c88e692cb7ba090
SeongbinPark <binni0515@gmail.com>
2022-10-06 14:30:45 +0900

    [#6] 사용자가 특정 게시물 조회시 조회 결과를 반환할 PostResponse DTO 생성

---
+++ src/main/java/com/seongbinpark/usedtradingplatform/post/dto/PostResponse.java
@@ -0,0 +1,45 @@
+package com.seongbinpark.usedtradingplatform.post.dto;
+
+import com.seongbinpark.usedtradingplatform.member.domain.entity.Address;
+import com.seongbinpark.usedtradingplatform.member.domain.entity.Location;
+import com.seongbinpark.usedtradingplatform.post.domain.entity.Post;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class PostResponse {
+
+    private final Long id;
+    private final String title;
+    private final String author;
+    private final String email;
+    private final String content;
+
+    private final String status;
+    private final String category;
+
+    private final Address address;
+    private final Location location;
+
+    private final LocalDateTime createdTime;
+    private final LocalDateTime modifiedTime;
+
+    public static PostResponse of(Post post) {
+        return PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .author(post.getAuthor().getNickname())
+                .email(post.getAuthor().getEmail())
+                .content(post.getContent())
+                .status(post.getStatus().getTradeStatus())
+                .category(post.getCategory().getCategoryName())
+                .address(post.getAddress())
+                .location(post.getLocation())
+                .build();
+    }
+}
