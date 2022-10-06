99ca2ccc6803e3499e4d42ec7d2f3765591a1ec2
SeongbinPark <binni0515@gmail.com>
2022-10-06 14:02:18 +0900

    [#6] 지역정보가 미등록된 사용자에 대한 예외 정의

--- src/main/java/com/seongbinpark/usedtradingplatform/commons/advice/ExceptionAdvice.java
+++ src/main/java/com/seongbinpark/usedtradingplatform/commons/advice/ExceptionAdvice.java
@@ -2,6 +2,8 @@ package com.seongbinpark.usedtradingplatform.commons.advice;
 
 import com.seongbinpark.usedtradingplatform.member.exception.MemberNotFoundException;
 import com.seongbinpark.usedtradingplatform.member.exception.UnauthenticatedAccessException;
+import com.seongbinpark.usedtradingplatform.post.exception.AreaInfoNotDefinedException;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,5 +22,10 @@ public class ExceptionAdvice {
     @ExceptionHandler(UnauthenticatedAccessException.class)
     public ResponseEntity<HttpStatus> unauthenticatedAccessException() {
         return RESPONSE_UNAUTHORIZED;
+    }
+
+    @ExceptionHandler(AreaInfoNotDefinedException.class)
+    public ResponseEntity<HttpStatus> areaInfoNotDefinedException(AreaInfoNotDefinedException e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN);
     }
 }
