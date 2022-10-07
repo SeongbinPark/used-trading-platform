0ecaab69daf347eedef9836b219dfdc6fc5a9a56
SeongbinPark <binni0515@gmail.com>
2022-10-07 17:47:41 +0900

    [#6] findPostById 에서 삭제여부를 검사해서 결과를 반환하도록 수정

--- src/main/java/com/seongbinpark/usedtradingplatform/post/repository/PostRepository.java
+++ src/main/java/com/seongbinpark/usedtradingplatform/post/repository/PostRepository.java
@@ -2,10 +2,13 @@ package com.seongbinpark.usedtradingplatform.post.repository;
 
 import com.seongbinpark.usedtradingplatform.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Optional;
 
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Query("SELECT p FROM Post p WHERE p.id = :postId AND p.removed = false")
     public Optional<Post> findPostById(Long id);
 }
