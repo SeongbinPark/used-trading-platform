ce54b6762065c55cd04d448df0dca6646e423c5b
SeongbinPark <binni0515@gmail.com>
2022-10-07 21:29:39 +0900

    [#9] AWS S3 파일 업로드 기능 구현

---
+++ src/main/java/com/seongbinpark/usedtradingplatform/image/service/StorageService.java
@@ -0,0 +1,11 @@
+package com.seongbinpark.usedtradingplatform.image.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface StorageService {
+
+    public String upload(MultipartFile file, String filename) throws IOException;
+
+}
